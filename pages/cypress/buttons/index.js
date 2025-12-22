95aa9aafc943c905ba802fb3f8014609eeb4d8ba
franklin <frank.fkn.23@gmail.com>
2025-12-21 22:29:33 -0300

    primeira tela para teste com botão

--- pages/cypress/buttons/index.js
+++ pages/cypress/buttons/index.js
@@ -1,3 +1,40 @@
+import styles from './index.module.css';
+import clsx from 'clsx';
+import { useState } from 'react';
+
 export default function Buttons() {
-  return <div>Página de botões</div>;
+  const [label, setLabel] = useState('Confirmar');
+
+  return (
+    <div className={clsx(styles.container)}>
+      <h1>Cenários de testes com botões</h1>
+      <h2>Caso 1</h2>
+      <div className={clsx(styles.coreContent)}>
+        <div className={clsx(styles.actionInfo)}>
+          <p className={clsx(styles.paragraph)}>
+            Mudar texto do botão ao clicar:
+          </p>
+          <button
+            data-test="button"
+            className={clsx(styles.button)}
+            onClick={() => setLabel('Clicou')}
+          >
+            {label}
+          </button>
+        </div>
+        <div className={clsx(styles.nextContainer)}>
+          <a href="./buttons/caso-2">
+            <button data-test="next-button" className={clsx(styles.button)}>
+              Próximo teste
+            </button>
+          </a>
+        </div>
+      </div>
+
+      {/* <h2>Botão com alert</h2>
+      <h2>Botão desabilitado</h2>
+      <h2>Botão que abre outra aba</h2>
+      <h2>Botão que muda a cor</h2> */}
+    </div>
+  );
 }
