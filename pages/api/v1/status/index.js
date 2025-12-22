ff285618e669376799bd364b2f50e9769042eca5
franklin <frank.fkn.23@gmail.com>
2025-12-22 16:51:52 -0300

    finalizando endpoint /status

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,8 +1,35 @@
 import database from 'infra/database.js';
 
 async function status(request, response) {
-  const result = await database.query('select 1 + 1 as sum;');
-  response.status(200).json({ mensage: 'It works!' });
+  const updatedAt = new Date().toISOString();
+  const data = await database.query('show server_version');
+
+  const databaseVersion = data.rows[0].server_version;
+
+  const connections = await database.query('show max_connections');
+  const maxConnections = parseInt(connections.rows[0].max_connections, 10);
+
+  const databaseName = process.env.POSTGRES_DB;
+
+  // count(1) ao invés de count(*) conta as linhas da tabela
+  // sem levar todos os objetos para a memória
+  const openConnections = await database.query({
+    text: 'select count(1)::int from pg_stat_activity where datname = $1;',
+    values: [databaseName],
+  });
+
+  const databaseOpenConnections = openConnections.rows[0].count;
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependecies: {
+      database: {
+        version: databaseVersion,
+        max_connections: maxConnections,
+        open_connections: databaseOpenConnections,
+      },
+    },
+  });
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,20 @@
 test('GET to /api/v1/status returns 200', async () => {
   const response = await fetch('http://localhost:3000/api/v1/status');
   expect(response.status).toBe(200);
+
+  const responseJson = await response.json();
+  const currentDate = new Date(responseJson.updated_at).toISOString();
+  expectedObject = {
+    updated_at: currentDate,
+    dependecies: {
+      database: {
+        version: '16.0',
+        max_connections: 100,
+        open_connections: 1,
+      },
+    },
+  };
+  expect(responseJson).toEqual(expectedObject);
+
+  // expect(responseDate.updated_at).toEqual(currentDate);
 });
