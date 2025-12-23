347372605a9813f5aeca85b1f87a920c5f27c365
franklin <frank.fkn.23@gmail.com>
2025-12-23 13:34:40 -0300

    ajustes de layout

--- pages/front/movies-list.js
+++ pages/front/movies-list.js
@@ -1,18 +1,49 @@
+import styles from './movies-list.module.css';
 export default function MoviesList() {
   return (
-    <div>
-      <h1>Melhores filmes</h1>
-      <h2>Top 3 filmes</h2>
-      <hr />
-      <h3>Antes que termine o dia</h3>
-      <p>Filme romântico pra macho que não sabe chorar.</p>
-      <h3>Zona mortal</h3>
-      <p>Filme de suspense com plot bom.</p>
-      <h3>O mistério das duas irmãs</h3>
-      <p>
-        Outro suspense com plot de fdp (acontece algo durante o filme todo, mas
-        você não percebe nem vai perceber ao ler isso).
-      </p>
+    <div className={styles.container}>
+      <div className={styles.header}>
+        <h1>Melhores filmes</h1>
+        <h2>Top 3 filmes</h2>
+        <hr />
+      </div>
+
+      <div className={styles.movies}>
+        <div className={styles.movieFrame}>
+          <h3>Antes que termine o dia</h3>
+          <div className={styles.poster}>
+            <img
+              className={styles.posterImg}
+              src="/images/if-only-poster.jpg"
+              alt="homem e mulher abraçados"
+            />
+          </div>
+          <div className={styles.p}>
+            Filme romântico pra macho que não sabe chorar.
+          </div>
+        </div>
+        <div className={styles.movieFrame}>
+          <h3>Zona mortal</h3>
+          <img
+            className={styles.posterImg}
+            src="/images/radius-poster.jpg"
+            alt="homem e mulher com alvo no fundo"
+          />
+          <div className={styles.p}>Filme de suspense com plot bom.</div>
+        </div>
+        <div className={styles.movieFrame}>
+          <h3>O mistério das duas irmãs</h3>
+          <img
+            className={styles.posterImg}
+            src="/images/uninvited-poster.jpg"
+            alt="sobra de mulher atrás da janela"
+          />
+          <div className={styles.p}>
+            Outro suspense com plot de fdp (acontece algo durante o filme todo,
+            mas você não percebe nem vai perceber ao ler isso).
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
