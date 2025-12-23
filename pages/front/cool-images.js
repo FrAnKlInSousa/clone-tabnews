e3c113a1c0e5878924a9d1cb2bef1df2d19f69ba
franklin <frank.fkn.23@gmail.com>
2025-12-23 13:49:37 -0300

    tela que carrega imagens aleatórias

---
+++ pages/front/cool-images.js
@@ -0,0 +1,18 @@
+export default function CoolImages() {
+  return (
+    <div>
+      <title>cool-images</title>
+
+      <div>
+        <img src="https://picsum.photos/500/300" />
+        <p>Imagem simples</p>
+      </div>
+      <div>
+        <a href="https://www.google.com" target="_blank">
+          <img src="https://picsum.photos/500/300" />
+        </a>
+        <p>Imagem com link</p>
+      </div>
+    </div>
+  );
+}
