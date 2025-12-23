ba1d06c61d2ee49c5d1fd6ee6f45da26e90c2916
franklin <frank.fkn.23@gmail.com>
2025-12-23 13:44:42 -0300

    adicionando links para imdb nas imagens

--- pages/front/movies-list.js
+++ pages/front/movies-list.js
@@ -12,11 +12,13 @@ export default function MoviesList() {
         <div className={styles.movieFrame}>
           <h3>Antes que termine o dia</h3>
           <div className={styles.poster}>
-            <img
-              className={styles.posterImg}
-              src="/images/if-only-poster.jpg"
-              alt="homem e mulher abraçados"
-            />
+            <a href="https://www.imdb.com/pt/title/tt0332136/" target="_blank">
+              <img
+                className={styles.posterImg}
+                src="/images/if-only-poster.jpg"
+                alt="homem e mulher abraçados"
+              />
+            </a>
           </div>
           <div className={styles.p}>
             Filme romântico pra macho que não sabe chorar.
@@ -24,20 +26,26 @@ export default function MoviesList() {
         </div>
         <div className={styles.movieFrame}>
           <h3>Zona mortal</h3>
-          <img
-            className={styles.posterImg}
-            src="/images/radius-poster.jpg"
-            alt="homem e mulher com alvo no fundo"
-          />
+          <a href="https://www.imdb.com/pt/title/tt6097798/" target="_blank">
+            <img
+              className={styles.posterImg}
+              src="/images/radius-poster.jpg"
+              alt="homem e mulher com alvo no fundo"
+            />
+          </a>
+
           <div className={styles.p}>Filme de suspense com plot bom.</div>
         </div>
         <div className={styles.movieFrame}>
           <h3>O mistério das duas irmãs</h3>
-          <img
-            className={styles.posterImg}
-            src="/images/uninvited-poster.jpg"
-            alt="sobra de mulher atrás da janela"
-          />
+          <a href="https://www.imdb.com/pt/title/tt0815245/" target="_blank">
+            <img
+              className={styles.posterImg}
+              src="/images/uninvited-poster.jpg"
+              alt="sobra de mulher atrás da janela"
+            />
+          </a>
+
           <div className={styles.p}>
             Outro suspense com plot de fdp (acontece algo durante o filme todo,
             mas você não percebe nem vai perceber ao ler isso).
