24a7d3ee5e28b79c9878151fe784e64fd185c198
franklin <frank.fkn.23@gmail.com>
2025-12-23 13:50:00 -0300

    atualizando tela com links do yt

--- pages/front/websites.js
+++ pages/front/websites.js
@@ -5,29 +5,81 @@ export default function Websites() {
       <hr />
       <ol>
         <li>
-          <a href="https://www.youtube.com" target="_blank">
-            YouTube
-          </a>
+          <span>Canais do YouTube</span>
+          <ul>
+            <li>
+              <a href="https://www.youtube.com/@PrimataFalante" target="_blank">
+                Primata falante
+              </a>
+            </li>
+            <li>
+              <a href="https://www.youtube.com/@Dunossauro" target="_blank">
+                Dunossauro
+              </a>
+            </li>
+            <li>
+              <a href="https://www.youtube.com/@3x3CustomTamar" target="_blank">
+                3x3Custom
+              </a>
+            </li>
+            <li>
+              <a href="https://www.youtube.com/user/karnaldo" target="_blank">
+                Religião levada a sério (sqn)
+              </a>
+            </li>
+            <li>
+              <a href="https://www.youtube.com/@AstrumBrasil" target="_blank">
+                Universo
+              </a>
+            </li>
+            <li>
+              <a href="https://www.youtube.com/@Bluciencia" target="_blank">
+                BluCiencia
+              </a>
+            </li>
+          </ul>
         </li>
         <li>
           <a href="https://tinywow.com" target="_blank">
-            TinyWow
+            Tinywow
           </a>
+          <span>
+            {' '}
+            - Site com vários utilitários de edição de arquivos (img, pdf etc)
+          </span>
         </li>
         <li>
           <a href="https://teachyourselfcs.com/" target="_blank">
-            Aprenda sozinho
+            Teachyourselfcs
           </a>
+          <span>
+            {' '}
+            - Várias fontes de conteúdo educativo na área da tecnologia.
+          </span>
         </li>
         <li>
           <a href="https://codebeautify.org/url-decode-string" target="_blank">
-            URL Decode
+            Codebeautify
           </a>
+          <span> - Site com variedade enorme de ferramentas.</span>
         </li>
         <li>
           <a href="https://www.diffchecker.com/pt/" target="_blank">
             Diff checker
           </a>
+          <span> - Site para comparar a diferença entre dois textos.</span>
+        </li>
+        <li>
+          <a href="https://picsum.photos/" target="_blank">
+            picsum
+          </a>
+          <span> - Imagens aleatórias</span>
+        </li>
+        <li>
+          <a href="https://flexboxfroggy.com/" target="_blank">
+            FlexBox Froggy
+          </a>
+          <span> - Site para treinar css com FlexBox</span>
         </li>
       </ol>
 
