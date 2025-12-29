26cbe2ef44b825387808ea0d5c78b4308412c267
franklin <frank.fkn.23@gmail.com>
2025-12-29 14:06:17 -0300

    mudando o nome da classe mainButton para button

--- components/button/button.js
+++ components/button/button.js
@@ -3,7 +3,7 @@ import styles from './index.module.css';
 export default function Button({ buttonText, dataTest }) {
   return (
     <>
-      <button data-test={dataTest} className={styles.mainButton}>
+      <button data-test={dataTest} className={styles.button}>
         {buttonText}
       </button>
     </>
