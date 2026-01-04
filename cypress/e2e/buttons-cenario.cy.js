9548763b984dbac61c37c03e88972bab8a1bd518
franklin <frank.fkn.23@gmail.com>
2026-01-04 19:50:30 -0300

    organizando parte do cenario

--- cypress/e2e/buttons-cenario.cy.js
+++ cypress/e2e/buttons-cenario.cy.js
@@ -1,19 +1,62 @@
-describe('dummy home page', () => {
+function buttonRedirect() {
+  cy.get('[data-test="button-option"]')
+    .as('button')
+    .should('have.text', 'Botões');
+  cy.get('@button').click();
+}
+
+function formRedirect() {
+  cy.get('[data-test="form-option"]')
+    .as('button')
+    .should('have.text', 'Formulários');
+
+  cy.get('@button').click();
+}
+
+function radiobuttonRedirect() {
+  cy.get('[data-test="radio-option"]')
+    .as('button')
+    .should('have.text', 'Radio buttons');
+  cy.get('@button').click();
+}
+
+function toggleRedirect() {
+  cy.get('[data-test="toggle-option"]')
+    .as('button')
+    .should('have.text', 'Toggles');
+  cy.get('@button').click();
+}
+
+function checkboxRedirect() {
+  cy.get('[data-test="checkbox-option"]')
+    .as('button')
+    .should('have.text', 'Checkboxes');
+  cy.get('@button').click();
+}
+
+function dropdownRedirect() {
+  cy.get('[data-test="dropdown-option"]')
+    .as('button')
+    .should('have.text', 'Dropdown');
+  cy.get('@button').click();
+}
+
+describe('check redirects on dummy home page', () => {
   beforeEach(() => {
+    // tela inicial
     cy.visit('/');
   });
 
-  it('check the buttons behaviors', () => {
-    // tela inicial
-    cy.get('[data-test="button-option"]')
-      .as('button-option')
-      .should('have.text', 'Botões');
+  it('check if button Formulários redirects to /forms', () => {
+    formRedirect();
+    cy.url().should('include', '/cypress/forms');
+  });
 
-    cy.get('@button-option').click();
-
-    // primeiro caso
+  it('check if button Botões redirects to /buttons', () => {
+    buttonRedirect();
     cy.url().should('include', '/cypress/buttons');
 
+    // depois separar a partir daki
     cy.get('[data-test="confirm-button"]')
       .as('confirm-button')
       .should('have.text', 'Confirmar')
@@ -35,5 +78,25 @@ describe('dummy home page', () => {
       .click()
       .should('have.text', 'Mudou de cor');
     cy.get('@confirm-button').should('have.css', 'background-color');
+  });
+
+  it('check if button Radio buttons redirects to /radio-buttons', () => {
+    radiobuttonRedirect();
+    cy.url().should('include', '/cypress/radio-buttons');
+  });
+
+  it('check if button Toggle redirects to /toggles', () => {
+    toggleRedirect();
+    cy.url().should('include', '/cypress/toggles');
+  });
+
+  it('check if button Checkboxes redirects to /checkboxes', () => {
+    checkboxRedirect();
+    cy.url().should('include', '/cypress/checkboxes');
+  });
+
+  it('check if button Dropdown redirects to /dropdowns', () => {
+    dropdownRedirect();
+    cy.url().should('include', '/cypress/dropdowns');
   });
 });
